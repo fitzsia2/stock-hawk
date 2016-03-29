8486875ff9d9c6b061b1bd347b5a3f6f7e0bb0f0
AndrewF <andrewsfitzsimons@gmail.com>
2016-03-28 19:50:14 -0700

    Added base url for YQL queries and the url to the yahoo finance table

--- app/src/main/java/com/sam_chordas/android/stockhawk/APIs/Yahoo.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/APIs/Yahoo.java
@@ -4,4 +4,6 @@ package com.sam_chordas.android.stockhawk.APIs;
  * Created by AndrewF on 3/27/2016.
  */
 public class Yahoo {
+    public static final String YQL_BASE = "https://query.yahooapis.com/v1/public/yql";
+    public static final String YAHOO_FINANCE_QUOTES_TABLE = "yahoo.finance.quotes";
 }
