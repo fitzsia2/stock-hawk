3e401491027f3598d8469c97b9de323fa1bde2cb
Andrew Fitzsimons <andrewsfitzsimons@gmail.com>
2016-04-02 11:16:32 -0700

    Feature
    Added radio buttons to details view and activity for viewing the stock's price over different time periods

--- app/src/main/java/com/sam_chordas/android/stockhawk/ui/DetailsActivity.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/ui/DetailsActivity.java
@@ -2,6 +2,9 @@ package com.sam_chordas.android.stockhawk.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 import com.github.mikephil.charting.charts.LineChart;
@@ -12,7 +15,7 @@ import java.text.SimpleDateFormat;
 import java.util.GregorianCalendar;
 import java.util.Locale;
 
-public class DetailsActivity extends AppCompatActivity {
+public class DetailsActivity extends AppCompatActivity implements View.OnClickListener {
     static final String LOG_TAG = DetailsActivity.class.getSimpleName();
     static final String STOCK_SYMBOL = "stock_symbol";
 
@@ -43,49 +46,49 @@ public class DetailsActivity extends AppCompatActivity {
         fht.execute(mStockSymbol, startDate, endDate);
 
         // Show that we're displaying a years worth of data
-//        RadioButton b = (RadioButton) findViewById(R.id.one_year_history_button);
-//        assert b != null;
-//        b.setChecked(true);
+        RadioButton b = (RadioButton) findViewById(R.id.one_year_history_button);
+        assert b != null;
+        b.setChecked(true);
     }
 
-//    @Override
-//    public void onClick(View v) {
-//
-//        // Get the current date
-//        String dateFormat = "yyyy-MM-dd";
-//        SimpleDateFormat sdf = new SimpleDateFormat(dateFormat, Locale.US);
-//        GregorianCalendar gregToday = new GregorianCalendar(Locale.US);
-//        String endDate = sdf.format(gregToday.getTime());
-//
-//
-//        RadioButton rb = (RadioButton) v;
-//        switch (rb.getId()) {
-//            case R.id.five_year_history_button:
-//                Log.v(LOG_TAG, "5Yr");
-//                gregToday.roll(GregorianCalendar.YEAR, -5);
-//                break;
-//            case R.id.one_year_history_button:
-//                Log.v(LOG_TAG, "1Yr");
-//                gregToday.roll(GregorianCalendar.YEAR, -1);
-//                break;
-//            case R.id.six_month_history_button:
-//                Log.v(LOG_TAG, "6Mo");
-//                gregToday.roll(GregorianCalendar.MONTH, -6);
-//                break;
-//            case R.id.one_week_history_button:
-//                Log.v(LOG_TAG, "1We");
-//                gregToday.roll(GregorianCalendar.DAY_OF_WEEK, -7);
-//                break;
-//            default:
-//                break;
-//        }
-//
-//
-//        // Set the start date based on selection
-//        String startDate = sdf.format(gregToday.getTime());
-//        FetchHistoryTask fht = new FetchHistoryTask((LineChart) findViewById(R.id.line_chart_view));
-//        fht.execute(mStockSymbol, startDate, endDate);
-//
-//        Log.v(LOG_TAG, "Start: " + startDate + "\nEnd: " + endDate);
-//    }
+    @Override
+    public void onClick(View v) {
+
+        // Get the current date
+        String dateFormat = "yyyy-MM-dd";
+        SimpleDateFormat sdf = new SimpleDateFormat(dateFormat, Locale.US);
+        GregorianCalendar gregToday = new GregorianCalendar(Locale.US);
+        String endDate = sdf.format(gregToday.getTime());
+
+
+        RadioButton rb = (RadioButton) v;
+        switch (rb.getId()) {
+            case R.id.five_year_history_button:
+                Log.v(LOG_TAG, "5Yr");
+                gregToday.roll(GregorianCalendar.YEAR, -5);
+                break;
+            case R.id.one_year_history_button:
+                Log.v(LOG_TAG, "1Yr");
+                gregToday.roll(GregorianCalendar.YEAR, -1);
+                break;
+            case R.id.six_month_history_button:
+                Log.v(LOG_TAG, "6Mo");
+                gregToday.roll(GregorianCalendar.MONTH, -6);
+                break;
+            case R.id.one_week_history_button:
+                Log.v(LOG_TAG, "1We");
+                gregToday.roll(GregorianCalendar.DAY_OF_WEEK, -7);
+                break;
+            default:
+                break;
+        }
+
+
+        // Set the start date based on selection
+        String startDate = sdf.format(gregToday.getTime());
+        FetchHistoryTask fht = new FetchHistoryTask((LineChart) findViewById(R.id.line_chart_view));
+        fht.execute(mStockSymbol, startDate, endDate);
+
+        Log.v(LOG_TAG, "Start: " + startDate + "\nEnd: " + endDate);
+    }
 }
